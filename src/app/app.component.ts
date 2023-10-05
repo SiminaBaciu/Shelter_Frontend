d474964909495cef7309d59dada4774b043a257f
Simina <baciu.gr.maria@student.utcluj.ro>
2023-10-05 19:59:17 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DS2023_30441_Baciu_Simina_Assignment_1_Frontend';
+}
